cae94c4e20d5f67d3a48b3021a80a7f25dc67e1d
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-13 21:22:44 -0500

    parte 2

---
+++ script2.js
@@ -0,0 +1,8 @@
+let n = parseInt(prompt("Digita un número"))
+if(n % 2==0){
+    console.log("Este número es par: ");
+    document.write("Este número es par: ", "<br>");
+}else{
+    console.log("Este número es Impar: ");
+    document.write("Este número es Impar: ", "<br>");
+}
