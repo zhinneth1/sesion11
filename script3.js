8632c84f440fb9510328d0a7788051671a391fdd
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-13 21:56:06 -0500

    parte 3

--- script3.js
+++ script3.js
@@ -1,7 +1,7 @@
 let monto = parseInt(prompt("Digita un valor de compra"));
 if(monto>100){
     let dsct10 = ((monto * 0.10)-monto);
-    document.write("Tu descuento es del 10% ","Valor total a pagar es de: $", dsct10, "<br>");
+    document.write("Tu descuento es del 10%, su valor total a pagar es de: $", dsct10, "<br>");
 }else{
-    document.write("No aplica a descuento, "," Monto a pagar es: $", monto , "<br>");
+    document.write("No aplica a descuento, su monto a pagar es: $", monto , "<br>");
 }
--- script4.js
+++ script4.js
@@ -1,1 +1,2 @@
-let n =  Math.floor
+let n =  Math.floor(math.random() * 10) + 1;
+let adivina = prompt("Adivina un número entre 1 y 10")
