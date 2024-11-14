d0cf102401fd26f94bea49e1e7fb11beeeaa9798
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-13 22:33:03 -0500

    parte 4

--- script4.js
+++ script4.js
@@ -1,6 +1,6 @@
 let n = Math.floor(math.random()*10)+1;
 let adivina = prompt("Adivina un número entre 1 y 10");
-if(adivina == n){
+if(adivina==n){
     document.write("Adivinastes, Felicidades");
 }else{
     document.write("No acertaste, vuelve a intentarlo la proxima. El numero es:  ", n);
