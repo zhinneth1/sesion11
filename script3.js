a66d699b8ea58c9536214494b580ec7f41428977
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-13 21:42:03 -0500

    parte 3

--- script3.js
+++ script3.js
@@ -1,8 +1,9 @@
 let monto = parseInt(prompt("Digita un valor de compra"));
 if(monto>100){
-    let dsct10 = ((monto * 0.10)-monto);
+    let dsct10 = (monto * 0.10);
     document.write("Tu descuento es del 10%", dsct10, "<br>");
 }else{
     document.write("No aplica a descuento", "<br>");
 }
-document.write("Valor total a pagar es: $",dsct10)
+let vlr = (monto - dsct10);
+document.write("Valor total a pagar es: $",dsct10);
