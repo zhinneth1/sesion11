ed77822caa72d721f4654d9afca4e6df331f33a1
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-13 21:45:45 -0500

    s

--- script3.js
+++ script3.js
@@ -3,5 +3,5 @@ if(monto>100){
     let dsct10 = ((monto * 0.10)-monto);
     document.write("Tu descuento es del 10% ", dsct10, "<br>");
 }else{
-    document.write("No aplica a descuento ", monto "<br>");
+    document.write("No aplica a descuento ", monto , "<br>");
 }
