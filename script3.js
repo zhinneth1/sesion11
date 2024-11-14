7b0d898feaa3a9f26cea21265b37df093121e49b
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-13 21:47:34 -0500

    se adiciona espacio

--- script3.js
+++ script3.js
@@ -1,7 +1,7 @@
 let monto = parseInt(prompt("Digita un valor de compra"));
 if(monto>100){
     let dsct10 = ((monto * 0.10)-monto);
-    document.write("Tu descuento es del 10% ", dsct10, "<br>");
+    document.write("Tu descuento es del 10% "," ", dsct10, "<br>");
 }else{
     document.write("No aplica a descuento ", monto , "<br>");
 }
